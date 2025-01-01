7470d39e50ef134eb4cb27d8fa79767db81954dc
prathamGulmire <102288747+prathamGulmire@users.noreply.github.com>
2025-01-01 20:04:30 +0530

    nothing much

--- src/main/java/com/prathGul/journalApp/controller/PublicCntroller.java
+++ src/main/java/com/prathGul/journalApp/controller/PublicCntroller.java
@@ -3,6 +3,8 @@ package com.prathGul.journalApp.controller;
 import com.prathGul.journalApp.entity.User;
 import com.prathGul.journalApp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -18,7 +20,13 @@ public class PublicCntroller {
     }
 
     @PostMapping("/create-user")
-    public void createUser(@RequestBody User user) {
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        if(user == null) {
+            return new ResponseEntity<>("please fill the body", HttpStatus.BAD_REQUEST);
+        }
+
         userService.saveNewUser(user);
+
+        return new ResponseEntity<>("user created", HttpStatus.CREATED);
     }
 }
